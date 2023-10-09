7937dd1fadca86ccd2cb8497e913b6fb4f404c09
Ozan <git@ozansap.com>
2023-10-09 16:00:32 +0300

    info text if no saved decks

--- src/routes/saved/+page.svelte
+++ src/routes/saved/+page.svelte
@@ -4,6 +4,9 @@
 </script>
 
 <div class="flex flex-col pb-32">
+	{#if $store_savedDecks.length === 0}
+		<h3 class="mx-auto mt-32 text-xl">You have no saved decks...</h3>
+	{/if}
 	{#each $store_savedDecks as deck, index}
 		<SavedDeck {deck} {index} />
 	{/each}
