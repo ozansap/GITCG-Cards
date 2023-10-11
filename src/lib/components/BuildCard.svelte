1e299f281946b042bbeb99e0ebc95f706aad18ab
Ozan <git@ozansap.com>
2023-10-11 19:27:02 +0300

    logic for arcane legend cards

--- src/lib/components/BuildCard.svelte
+++ src/lib/components/BuildCard.svelte
@@ -8,15 +8,16 @@
 	export let cardData: AnyCardData;
 	export let index: number;
 
+	const singleCopy = cardType === 'characters' || (330000 < cardData.id && cardData.id < 331000);
+
 	function handle() {
+		if (unavailable) return;
 		let card = { id: cardData.id, name: cardData.name };
 
 		if (cardType === 'characters') {
-			if ($store_buildDeck.cards.characters.filter((x) => x.id === card.id).length === 1) return;
 			if ($store_buildDeck.cards.characters.length === 3) return;
 			store_buildDeck.addCharacter(card);
 		} else {
-			if ($store_buildDeck.cards.actions.filter((x) => x.id === card.id).length === 2) return;
 			if ($store_buildDeck.cards.actions.length === 30) return;
 			store_buildDeck.addAction(card);
 		}
@@ -25,7 +26,7 @@
 	}
 
 	$: count = $store_buildDeck.cards[cardType].filter((x) => x.id === cardData.id).length;
-	$: unavailable = cardType === 'characters' ? count === 1 : count === 2;
+	$: unavailable = singleCopy ? count === 1 : count === 2;
 </script>
 
 <button on:click={handle} class="flex items-center justify-center rounded-md border-2 border-transparent hover:!border-white">
