98f3fcdd6955ef86178213b7888a626c7fddb2f1
Ozan <git@ozansap.com>
2023-10-11 04:41:30 +0300

    build option in home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { goto } from '$app/navigation';
 	import SVG_code from '$lib/svg/code.svelte';
 	import SVG_image from '$lib/svg/image.svelte';
+	import SVG_add from '$lib/svg/add.svelte';
 	import { decode } from '$lib/utils/Share.js';
 	import { store_buildDeck } from '$lib/utils/stores.js';
 	import { toast } from '$lib/utils/toast.js';
@@ -18,6 +19,20 @@
 </script>
 
 <div class="mx-4 mt-16 flex flex-col items-center">
+	<h1 class="mb-8 text-3xl font-bold">Build your own deck</h1>
+	<div class="flex w-40 flex-col items-center gap-8 text-xl">
+		<button on:click={() => goto('/build')} class="flex w-full items-center rounded-lg bg-color_primary px-2 py-1">
+			<div class="w-6"><SVG_add /></div>
+			<div class="m-auto">Build</div>
+		</button>
+	</div>
+
+	<div class="my-8 flex w-full items-center">
+		<div class="mr-4 flex-grow border-t-2 border-color_text" />
+		<p class="text-2xl font-bold text-color_text">or</p>
+		<div class="ml-4 flex-grow border-t-2 border-color_text" />
+	</div>
+
 	<h1 class="mb-8 text-3xl font-bold">Import deck from</h1>
 	<div class="flex w-40 flex-col items-center gap-8 text-xl">
 		<button on:click={importCode} class="flex w-full items-center rounded-lg bg-color_primary px-2 py-1">
